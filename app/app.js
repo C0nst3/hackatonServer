3be686ebaa0889b48458943835deb9783114fab6
mgiambi <michaelgiambi2@gmail.com>
2017-05-15 11:19:40 +0200

    v 1.3

--- app/app.js
+++ app/app.js
@@ -4,47 +4,62 @@ var app = angular.module("Jarrupalat", []);
 app.controller("inputController", ["$scope", "JarrupalatService", 
 	function($scope, JarrupalatService) {
 
-	$scope.txtResults = "";
-	$scope.showResults = false;
+		$scope.txtResults = "";
+		$scope.showResults = false;
+		var strings = ["Training in progress", "Training in progress.", "Training in progress..", "Training in progress..."];
+		var index = 0;
 
-	$scope.inputFunction = function() {
-		
-		if(!(typeof $scope.txtFormula === undefined)) {
-			var arrToParse = $scope.txtFormula.split(',');
+		$scope.inputFunction = function() {
 
-			var dataToStore=new Object();
-			dataToStore["Formula"]=arrToParse[0];
-			dataToStore["List"]=[];
-			for(var j=1; j<arrToParse.length; j=j+3) {
-				dataToStore["List"].push({
-					Materia_prima:arrToParse[j],
-					Quantita:arrToParse[j+1],
-					Gruppo:arrToParse[j+2]
-				});
+			if(!(typeof $scope.txtFormula === undefined)) {
+				var arrToParse = $scope.txtFormula.split(',');
+
+				var dataToStore=new Object();
+				dataToStore["Formula"]=arrToParse[0];
+				dataToStore["List"]=[];
+				for(var j=1; j<arrToParse.length; j=j+3) {
+					dataToStore["List"].push({
+						Materia_prima:arrToParse[j],
+						Quantita:arrToParse[j+1],
+						Gruppo:arrToParse[j+2]
+					});
+				}
+
+				console.log(dataToStore);
+				$scope.txtResults = dataToStore;
+				$scope.showResults = true;
+
 			}
+		}
 
-			console.log(dataToStore);
-			$scope.txtResults = dataToStore;
+		$scope.loadParameters = function() {
+
+			$scope.txtResults = "Parameters loaded."
 			$scope.showResults = true;
 
 		}
-	}
 
-	$scope.loadParameters = function() {
+		$scope.startTraining = function() {
 
-		$scope.txtResults = "Parameters loaded."
-		$scope.showResults = true;
+			/*JarrupalatService.mergeCSV(function(res) {});*/
+			changeText();
 
-	}
+		}
 
-	$scope.startTraining = function() {
-		JarrupalatService.mergeCSV(function(res) {
+		function changeText() {
 
-			$scope.txtResults = "Training complete."
+			index++;
+
+			if(index >= strings.length) {
+				index = 0;
+			}
+
+			$scope.txtResults = strings[index];
+
 			$scope.showResults = true;
 			console.log(res);
+		}
 
-		});
-	}
+		setInterval(changeText,10);
 
-}]);
+	}]);
