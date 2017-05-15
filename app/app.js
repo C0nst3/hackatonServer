67c159d31956b66cba9d8b9c495274147c028055
unknown <michaelgiambi2@gmail.com>
2017-05-15 09:51:38 +0200

    conve puzza

---
+++ app/app.js
@@ -0,0 +1,46 @@
+var app = angular.module('app', []);
+
+app.controller("inputController", ["$scope", "jarrupalatService", function($scope, jarrupalatService) {
+
+	$scope.txtResults = "";
+	$scope.showResults = false;
+
+	$scope.inputFunction = function() {
+		
+		if(!(typeof $scope.txtFormula === undefined)) {
+			var arrToParse = $scope.txtFormula.split(',');
+
+			var dataToStore=new Object();
+			dataToStore["Formula"]=arrToParse[0];
+			dataToStore["List"]=[];
+			for(var j=1; j<arrToParse.length; j=j+3) {
+				dataToStore["List"].push({
+					Materia_prima:arrToParse[j],
+					Quantita:arrToParse[j+1],
+					Gruppo:arrToParse[j+2]
+				});
+			}
+
+			console.log(dataToStore);
+			$scope.txtResults = dataToStore;
+			$scope.showResults = true;
+
+		}
+	}
+
+	$scope.loadParameters = function() {
+
+		$scope.txtResults = "Parameters loaded."
+		$scope.showResults = true;
+
+	}
+
+	$scope.startTraining = function() {
+		jarrupalatService.mergeCSV(function(res) {
+
+			console.log(res);
+
+		});
+	}
+
+}]);
