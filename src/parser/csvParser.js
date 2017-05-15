79fb0ea65d85b451c16923a761b355b87b298deb
C0nst3 <stefano.convertino@gmail.com>
2017-05-15 11:17:38 +0200

    added csvParser

--- src/parser/csvParser.js
+++ src/parser/csvParser.js
@@ -5,6 +5,11 @@ var parse = require('csv-parse');
 var fs = require('fs');
 var cloudant = require('cloudant');
 var hat=require('hat');
+//Parte del db:
+var dbCredentials = {
+    dbName: 'indilium'
+};
+var db;
 
 // funzione per leggere le credenziali di accesso del db
 function getDBCredentialsUrl(jsonData) {
@@ -19,19 +24,12 @@ function getDBCredentialsUrl(jsonData) {
     }
 }
 
-//Parte del db:
-var dbCredentials = {
-    dbName: 'indilium'
-};
-var db;
-
 function initDBConnection() {
     //When running on Bluemix, this variable will be set to a json object
     //containing all the service credentials of all the bound services
     if (process.env.VCAP_SERVICES) {
         dbCredentials.url = getDBCredentialsUrl(process.env.VCAP_SERVICES);
     } else { //When running locally, the VCAP_SERVICES will not be set
-
         // When running this app locally you can get your Cloudant credentials
         // from Bluemix (VCAP_SERVICES in "cf env" output or the Environment
         // Variables section for an app in the Bluemix console dashboard).
@@ -55,43 +53,17 @@ function initDBConnection() {
 }
 initDBConnection();
 
-
-//var mongoose = require('mongoose');
-//mongoose.connect('mongodb://127.0.0.1:27017/hackatondb');
-//
-//// definition of the model
-//var prove = mongoose.model('prove',
-//	{
-//			IdMaterial:Number,
-//			Usura_mat_mm:Number,
-//			Usura_mat_g:Number,
-//			Usura_disco_g:Number,
-//			Raggio:Number,
-//			Durezza:Number,
-//			List: [{
-//				Materia_prima: Number,
-//				Quantità: Number,
-//				Gruppo: Number
-//			}]
-//	}
-//);
-
-
 // Per salvare un documento
 var saveDocument = function(arrToSave,res) {
-
-	//console.log("array to save:"+arrToSave);
     var id = hat();
-    db.insert(arrToSave, id, function(err, doc) {
+    db.insert({ListOfToSave:arrToSave}, id, function(err, doc) {
         if (err) {
         	res.json("KO|"+err+": "+doc);
         }else{
-        	res.json("OK");
+        	res.json("OK|"+id);
         }   
     });
 }
-
-
 
 CsvParser.prototype.parse = function(proveCsvPath,materialCsvPath,res){
 	var arr_csvReaded=[], arr_jsonProveToStore=[], arr_jsonMaterialToStore=[];
@@ -112,14 +84,6 @@ CsvParser.prototype.parse = function(proveCsvPath,materialCsvPath,res){
 										
 									});
 									saveDocument(arr_jsonProveToStore,res);
-									/*var tmpProva = new prove(arr_jsonMaterialToStore);
-										tmpProva.save(function(err, data){
-											if(err){
-												res.json("KO|"+err);
-											}else{
-												res.json("OK");
-											}
-									});*/
 								});
 						}
 					});
@@ -132,3 +96,26 @@ CsvParser.prototype.parse = function(proveCsvPath,materialCsvPath,res){
 
 // export the class 
 module.exports = new CsvParser();
+
+
+
+
+//var mongoose = require('mongoose');
+//mongoose.connect('mongodb://127.0.0.1:27017/hackatondb');
+//
+//// definition of the model
+//var prove = mongoose.model('prove',
+//  {
+//          IdMaterial:Number,
+//          Usura_mat_mm:Number,
+//          Usura_mat_g:Number,
+//          Usura_disco_g:Number,
+//          Raggio:Number,
+//          Durezza:Number,
+//          List: [{
+//              Materia_prima: Number,
+//              Quantità: Number,
+//              Gruppo: Number
+//          }]
+//  }
+//);
